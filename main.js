9752e1d21340edac17d27ecd82e956d65446aa38
Aldodev01 <aldodevox@gmail.com>
2020-09-30 20:43:17 +0700

    Upload file domevent

---
+++ main.js
@@ -0,0 +1,20 @@
+
+
+// Kita buat sebuah fungsi untuk
+// memunculkan MODAL
+
+const showModal = ()=>{
+    // kita dapatkan element MODAL
+    // yang mau dimunculkan
+    let modal = document.getElementById('modalMenu')
+
+    // kita atur styling element modalnya
+    modal.style.display = 'flex'
+
+}
+
+const closeModal = ()=>{
+    let modal = document.getElementById
+    ('modalMenu')
+    modal.style.display = 'none'
+}
